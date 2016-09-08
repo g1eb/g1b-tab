b5bcb5fa3f92823ce789ddc459189f2e700ef7f7
Gleb <git@gleb.nl>
2016-09-08 17:48:15 +0200

    Restructured the script for the initialization bit.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
-chrome.omnibox.onInputChanged.addListener(function (q) {
-  var el = document.createElement('span');
-  document.body.appendChild(el);
-  var updateElement = function(q) {
-    var offset = Math.floor(Math.random() * 50);
-    var duration = Math.floor(Math.random() * 10);
-    var size = 15 + 10 - duration;
-    el.innerHTML = '<span style="right:'+offset+'vw; font-size: '+size+'px; animation-duration:'+duration+'s">'+q.slice(-1)+'</span>';
-    setTimeout(function () {
-      updateElement(q);
-    }, duration * 1000);
-  };
-  updateElement(q);
-});
+'use strict';
 
-chrome.omnibox.onInputEntered.addListener(function (q) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: 'https://google.com/search?q='+q});
-  });
-});
+var tab = {
+
+  init: function () {
+    tab.setOmniboxListeners();
+  },
+
+  setOmniboxListeners: function () {
+    chrome.omnibox.onInputChanged.addListener(function (q) {
+      var el = document.createElement('span');
+      document.body.appendChild(el);
+      var updateElement = function(q) {
+        var offset = Math.floor(Math.random() * 50);
+        var duration = Math.floor(Math.random() * 10);
+        var size = 15 + 10 - duration;
+        el.innerHTML = '<span style="right:'+offset+'vw; font-size: '+size+'px; animation-duration:'+duration+'s">'+q.slice(-1)+'</span>';
+        setTimeout(function () {
+          updateElement(q);
+        }, duration * 1000);
+      };
+      updateElement(q);
+    });
+
+    chrome.omnibox.onInputEntered.addListener(function (q) {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.update(tabs[0].id, {url: 'https://google.com/search?q='+q});
+      });
+    });
+  },
+
+};
+
+document.addEventListener('DOMContentLoaded', tab.init);
